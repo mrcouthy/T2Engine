b46fa261a1e5db7d38afa2a225e41ea81cda7d8d
mrcouthy <mrcouthy@gmail.com>
2015-04-01 18:08:06 +0545

    Adding t2 engine

---
+++ T2Engine/Core/Formats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2Engine.Core
+{
+   public static class Formats
+    {
+       public static string[] GetFormats( string formatFile)
+        {
+            var formats = new List<string>();
+            var splitedFormat = System.IO.File.ReadAllLines(formatFile);
+            foreach (var f in splitedFormat)
+            {
+                formats.Add(f.Trim());
+            }
+            return formats.ToArray();
+        }
+    }
+}
