2d8da38baa3a0f9d3d31171d57084d2a3bbee900
Dj <mrcouthy@gmail.com>
2015-04-02 07:44:14 +0545

    Adding settings with relative path capability. Default is same as app folder.

--- T2Engine/Program.cs
+++ T2Engine/Program.cs
@@ -15,13 +15,20 @@ namespace T2Engine
 
         static void Main(string[] args)
         {
-
             //string template = "Hello @Model.Name, welcome to RazorEngine!@1+2";
             //var result =Engine.Razor.RunCompile(template, "templateKey", null, new { Name = "World" });
+            string workingFolder = Properties.Settings.Default.WorkingFolder;
+            
+            if (workingFolder.Length != 0 && !Directory.Exists(workingFolder))
+            {
+                Console.WriteLine("Working directory {0} not found ! Using Default folder.", workingFolder);
+                workingFolder = "";
+            }
+            Directory.SetCurrentDirectory(workingFolder);
+            var inputFiles = Directory.GetFiles("Input/");
+            var formatDirectories = Directory.GetDirectories("Templates/");
 
-            var InputFiles = Directory.GetFiles("Input/");
-            var FormatDirectories = Directory.GetDirectories("Templates/");
-            new Transform().Loop(FormatDirectories, InputFiles);
+            new Transform().Loop(formatDirectories, inputFiles);
         }
 
 
