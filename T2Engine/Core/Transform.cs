dd9e83f51c71b54928a4c3b9849c23bf6f14ddf4
Dj <mrcouthy@gmail.com>
2015-04-02 11:24:24 +0545

    Make it runnable form windows run ... //set environement variable path ="exe path folder" Clean up

--- T2Engine/Core/Transform.cs
+++ T2Engine/Core/Transform.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using T2Engine.Helpers;
 namespace T2Engine.Core
 {
--- T2Engine/Program.cs
+++ T2Engine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Net.Mime;
 using T2Engine.Core;
 
 namespace T2Engine
@@ -9,6 +10,8 @@ namespace T2Engine
 
         static void Main(string[] args)
         {
+            //set environement variable path ="exe path folder"
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
             //string template = "Hello @Model.Name, welcome to RazorEngine!@1+2";
             //var result =Engine.Razor.RunCompile(template, "templateKey", null, new { Name = "World" });
             string workingFolder = Properties.Settings.Default.WorkingFolder;
@@ -16,9 +19,11 @@ namespace T2Engine
             if (workingFolder.Length != 0 && !Directory.Exists(workingFolder))
             {
                 Console.WriteLine("Working directory {0} not found ! Using Default folder.", workingFolder);
+                Console.ReadLine();
                 workingFolder = "";
             }
             Directory.SetCurrentDirectory(workingFolder);
+            Console.WriteLine(Directory.GetCurrentDirectory());
             var inputFiles = Directory.GetFiles("Input/");
             var formatDirectories = Directory.GetDirectories("Templates/");
 
