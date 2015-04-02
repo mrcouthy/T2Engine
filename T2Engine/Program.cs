2bb3c2931faae07ab142282b0cee42fde90db67b
Dj <mrcouthy@gmail.com>
2015-04-02 11:04:45 +0545

    Removing reazorengine package

--- T2Engine/Program.cs
+++ T2Engine/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.SqlServer.Server;
-using RazorEngine;
 using T2Engine.Core;
-using RazorEngine.Templating;
+
 namespace T2Engine
 {
     class Program
