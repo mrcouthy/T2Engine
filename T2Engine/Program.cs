65b5e91ba0322cf4a6f045ace5f75cb4bebd5e21
mrcouthy <mrcouthy@gmail.com>
2015-04-03 13:14:07 +0545

    Adding helper excel with macro to make csv. Ignore _ starting formats feature added

--- T2Engine/Program.cs
+++ T2Engine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net.Mime;
 using T2Engine.Core;
 
@@ -15,7 +16,7 @@ namespace T2Engine
             //string template = "Hello @Model.Name, welcome to RazorEngine!@1+2";
             //var result =Engine.Razor.RunCompile(template, "templateKey", null, new { Name = "World" });
             string workingFolder = Properties.Settings.Default.WorkingFolder;
-            
+
             if (workingFolder.Length != 0 && !Directory.Exists(workingFolder))
             {
                 Console.WriteLine("Working directory {0} not found ! Using Default folder.", workingFolder);
@@ -24,12 +25,37 @@ namespace T2Engine
             }
             Directory.SetCurrentDirectory(workingFolder);
             Console.WriteLine(Directory.GetCurrentDirectory());
+            t2();
+            // t3();
+        }
+
+        private static void t2()
+        {
+            var inputFiles = Directory.GetFiles("Input/");
+            var formatDirectories = Directory.GetDirectories("Templates/");
+            var fd= ( from string format in formatDirectories
+                      where !Path.GetFileName( format).StartsWith( "_")
+                                select format).ToArray();
+            new Transform().Loop(fd, inputFiles);
+        }
+        private static void t3()
+        {
             var inputFiles = Directory.GetFiles("Input/");
             var formatDirectories = Directory.GetDirectories("Templates/");
 
-            new Transform().Loop(formatDirectories, inputFiles);
+            foreach (var inputFile in inputFiles)
+            {
+                var a = File.ReadAllLines(inputFile);
+                //  string outputFileName = FileRelated.GetFileFormatedFileName(inputFile, formatDirectory);
+                foreach (var b in a)
+                {
+                    string filname = b.Substring(0, b.IndexOf(".")) + ".csv";
+                    string ins = File.ReadAllText(@"D:\teng\in.csv");
+                    File.WriteAllText("Output/" + filname, ins);
+                }
+
+
+            }
         }
-
-
     }
 }
