dca44308947384dea2ef67824a838ebcc9cd859a
mrcouthy <mrcouthy@gmail.com>
2015-04-08 18:04:02 +0545

    Adding CLAP

--- T2Engine/Program.cs
+++ T2Engine/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Net.Mime;
+using CLAP;
 using T2Engine.Core;
 
 namespace T2Engine
@@ -25,20 +26,34 @@ namespace T2Engine
             }
             Directory.SetCurrentDirectory(workingFolder);
             Console.WriteLine(Directory.GetCurrentDirectory());
-            t2();
-            // t3();
-        }
 
-        private static void t2()
+            Parser.Run<TEng>(args);
+            //TEng.Transform();
+            //TEng.t3();
+        }
+    }
+
+    public class TEng
+    {
+        [Empty]
+        public static void Transform()
         {
             var inputFiles = Directory.GetFiles("Input/");
             var formatDirectories = Directory.GetDirectories("Templates/");
-            var fd= ( from string format in formatDirectories
-                      where !Path.GetFileName( format).StartsWith( "_")
-                                select format).ToArray();
+            var fd = (from string format in formatDirectories
+                      where !Path.GetFileName(format).StartsWith("_")
+                      select format).ToArray();
             new Transform().Loop(fd, inputFiles);
         }
-        private static void t3()
+
+        [Verb]
+        public static void Append()
+        {
+            Console.WriteLine("T3 append");
+        }
+
+        [Verb]
+        public static void CreateFiles()
         {
             var inputFiles = Directory.GetFiles("Input/");
             var formatDirectories = Directory.GetDirectories("Templates/");
@@ -53,8 +68,6 @@ namespace T2Engine
                     string ins = File.ReadAllText(@"D:\teng\in.csv");
                     File.WriteAllText("Output/" + filname, ins);
                 }
-
-
             }
         }
     }
