2bed9678e3a81f4a222153f7f003e632f889e5ac
mrcouthy <mrcouthy@gmail.com>
2015-04-16 21:00:16 +0545

    Adding duplicator

---
+++ Duplicator/FrmDupliactor.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Duplicator
+{
+    partial class Duplicator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtDelimiter = new System.Windows.Forms.TextBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btnWithDelimiter = new System.Windows.Forms.Button();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label5
+            // 
+            this.Label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(371, 273);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(39, 13);
+            this.Label5.TabIndex = 11;
+            this.Label5.Text = "Output";
+            // 
+            // Label4
+            // 
+            this.Label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(366, 12);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(31, 13);
+            this.Label4.TabIndex = 12;
+            this.Label4.Text = "Input";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(12, 12);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(27, 13);
+            this.Label3.TabIndex = 13;
+            this.Label3.Text = "Find";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(12, 55);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(47, 13);
+            this.Label2.TabIndex = 14;
+            this.Label2.Text = "Replace";
+            // 
+            // Label1
+            // 
+            this.Label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(371, 458);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(47, 13);
+            this.Label1.TabIndex = 15;
+            this.Label1.Text = "Delimiter";
+            // 
+            // txtDelimiter
+            // 
+            this.txtDelimiter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtDelimiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDelimiter.Location = new System.Drawing.Point(424, 448);
+            this.txtDelimiter.Name = "txtDelimiter";
+            this.txtDelimiter.Size = new System.Drawing.Size(66, 26);
+            this.txtDelimiter.TabIndex = 9;
+            this.txtDelimiter.Text = ",";
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(15, 32);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(351, 20);
+            this.txtFind.TabIndex = 10;
+            // 
+            // btnWithDelimiter
+            // 
+            this.btnWithDelimiter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnWithDelimiter.Location = new System.Drawing.Point(697, 449);
+            this.btnWithDelimiter.Name = "btnWithDelimiter";
+            this.btnWithDelimiter.Size = new System.Drawing.Size(111, 30);
+            this.btnWithDelimiter.TabIndex = 7;
+            this.btnWithDelimiter.Text = "&Ok";
+            this.btnWithDelimiter.UseVisualStyleBackColor = true;
+            this.btnWithDelimiter.Click += new System.EventHandler(this.btnWithDelimiter_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtList.Location = new System.Drawing.Point(12, 71);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(351, 408);
+            this.txtList.TabIndex = 4;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.Location = new System.Drawing.Point(369, 298);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(439, 144);
+            this.txtOut.TabIndex = 5;
+            // 
+            // txtText
+            // 
+            this.txtText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtText.Location = new System.Drawing.Point(369, 32);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(439, 238);
+            this.txtText.TabIndex = 6;
+            // 
+            // Duplicator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 491);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtDelimiter);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnWithDelimiter);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtText);
+            this.Name = "Duplicator";
+            this.Text = "Duplicator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtDelimiter;
+        internal System.Windows.Forms.TextBox txtFind;
+        internal System.Windows.Forms.Button btnWithDelimiter;
+        internal System.Windows.Forms.TextBox txtList;
+        internal System.Windows.Forms.TextBox txtOut;
+        internal System.Windows.Forms.TextBox txtText;
+
+    }
+}
+
---
+++ Duplicator/FrmDupliactor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Duplicator
+{
+    public partial class Duplicator : Form
+    {
+        public Duplicator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWithDelimiter_Click(object sender, EventArgs e)
+        {
+            string lst = txtList.Text;
+            string find = txtFind.Text;
+            string textS = txtText.Text;
+            string delimiter = txtDelimiter.Text;
+
+            string outt = String.Empty;
+            string[] lstitemS;
+            string temp = "";
+
+            var findS = find.Split(new string[] { delimiter }, StringSplitOptions.None);
+            var listS = lst.Trim().Split(new string[] { System.Environment.NewLine }, StringSplitOptions.None);
+            for (int i = 0; (i <= (listS.Length - 1)); i++)
+            {
+                lstitemS = listS[i].Trim().Split(new string[] { delimiter }, StringSplitOptions.None);
+                temp = textS;
+                for (int j = 0; (j <= (lstitemS.Length - 1)); j++)
+                {
+                    temp = temp.Replace(findS[j], lstitemS[j]);
+                }
+                outt = (outt + (temp + "\r\n"));
+            }
+            Clipboard.SetText(outt);
+            txtOut.Text = outt;
+        }
+    }
+}
